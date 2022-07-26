c36b15419718a15ec3db1f2fb3afd56d40144017
FabianXRL16 <fabianpacherres@gmail.com>
2022-07-26 15:42:47 -0500

    install bootstrap

--- src/main.js
+++ src/main.js
@@ -3,7 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { BootstrapVue, BIconPlus, BIconChevronLeft } from "bootstrap-vue";
+
+Vue.use(BootstrapVue);
+
+
+Vue.component("BIconPlus", BIconPlus);
+Vue.component("BIconChevronLeft", BIconChevronLeft);
+
 Vue.config.productionTip = false
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 new Vue({
   router,
